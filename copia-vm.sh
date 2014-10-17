17045dade3a4599bcd598cf5f3d1c5ddcb0ff9df
Laercio da Silva Motta <laerciomasala@gmail.com>
2014-10-16 22:30:36 -0300

    Variavel nova

--- copia-vm.sh
+++ copia-vm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #by admlaercio
 
+serv_nfs="172.25.55.55:/mnt/nfs-export"
+
 do_erro() {
  echo ">>> Erro! $1"
  echo
@@ -11,5 +13,5 @@ do_erro() {
 
 test -z $1 && do_erro "Falta nome Host hospedeiro"
 test -z $2 && do_erro "Falta nome VM"
-time LIBGUESTFS_TRACE=1 LIBGUESTFS_DEBUG=1 virt-v2v -o rhev -ic qemu+ssh://root@$1/system -osd 172.25.55.55:/mnt/nfs-export $2  2>&1 | tee /tmp/virt-v2v-$2-`date +%d%m%y%H%M`.log
+time LIBGUESTFS_TRACE=1 LIBGUESTFS_DEBUG=1 virt-v2v -o rhev -ic qemu+ssh://root@$1/system -osd $serv_nfs $2  2>&1 | tee /tmp/virt-v2v-$2-`date +%d%m%y%H%M`.log
 
